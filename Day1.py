ff063b011862d20afe96a78109ccf43b74353f8f
eayush97-hash <e.ayush97@gmail.com>
2025-08-18 19:13:36 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,10 @@
+# Day 1
+# Today i learned about print and statements. If i start a statement with a hash,
+# It will execute as a comment.
+# And by writing print in i can tell the computer that i just want to print the texts under "".
+print("Hello World") #It is a example of that print command.
+
+# I also learned that if i put \n at middlen of any string(the text under ""), it will print the text after this in a new line.
+print("Hello\nWorld") #It is a example of that \n command.
+
+# Thats all for today, as my fisrt session of python programming.
