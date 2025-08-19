1d0cfbadb9b9fab834bacf8e0cd5ec1369bc2aaa
eayush97-hash <e.ayush97@gmail.com>
2025-08-19 17:01:48 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,8 @@
+# Today i learned about variables, data types, and basic operations in Python
+print ("Hello, World!") # This is a string example
+print (5) # This is an integer example
+print (3.14) # This is a float example
+print (True) # This is a boolean example
+
+# If i want to check data types, i can use the type() function
+print(type("Hello, World!")) # Like this
